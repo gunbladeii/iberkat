d87a0f34c0e0d397715764bf94c2b1bfeed04390
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-06-11 21:21:13 +0800

    Update test2.php

--- test2.php
+++ test2.php
@@ -53,7 +53,12 @@
       });
     </script>
 
-    <input type="text" name="scanInput" id="scanInput">
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <span class="input-group-text" id="basic-addon1">Result</span>
+      </div>
+      <input type="text" class="form-control" name="scanInput" id="scanInput" aria-label="Username" aria-describedby="basic-addon1">
+    </div>
 
     </div>
   </div>
