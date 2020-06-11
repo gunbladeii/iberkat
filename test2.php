9eb7c83a9705406222cce6557c6790794df3d2fe
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-06-11 21:05:28 +0800

    Update test2.php

--- test2.php
+++ test2.php
@@ -33,43 +33,27 @@
   <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
       <div class="col">
         <script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
-        <h1><a href="https://learncodeweb.com/phonegap/how-to-use-instascan-an-html5-qr-scanner/">How to use Instascan an HTML5 QR scanner</a></h1>
         <div class="col-sm-12">
-          <video id="preview" class="p-1 border" style="width:100%;"></video>
+          <video id="preview" class="p-1 border" style="height:100%;width:100%;"></video>
         </div>
-        <script type="text/javascript">
-          var scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5, mirror: false });
-          scanner.addListener('scan',function(content){
-            alert(content);
-            //window.location.href=content;
-          });
-          Instascan.Camera.getCameras().then(function (cameras){
-            if(cameras.length>0){
-              scanner.start(cameras[0]);
-              $('[name="options"]').on('change',function(){
-                if($(this).val()==1){
-                  if(cameras[0]!=""){
-                    scanner.start(cameras[0]);
-                  }else{
-                    alert('No Front camera found!');
-                  }
-                }else if($(this).val()==2){
-                  if(cameras[1]!=""){
-                    scanner.start(cameras[1]);
-                  }else{
-                    alert('No Back camera found!');
-                  }
-                }
-              });
-            }else{
-              console.error('No cameras found.');
-              alert('No cameras found.');
-            }
-          }).catch(function(e){
-            console.error(e);
-            alert(e);
-          });
-        </script>
+
+         <script type="text/javascript">
+      let scanner = new Instascan.Scanner({ video: document.getElementById('preview'), scanPeriod: 5, mirror: false });
+      scanner.addListener('scan', function (content) {
+        alert(content);
+      });
+      Instascan.Camera.getCameras().then(function (cameras) {
+        if (cameras.length > 0) {
+          scanner.start(cameras[1]);
+        } else {
+          console.error('No cameras found.');
+        }
+      }).catch(function (e) {
+        console.error(e);
+      });
+    </script>
+
+       
         <div class="btn-group btn-group-toggle mb-5" data-toggle="buttons">
           <label class="btn btn-primary active">
           <input type="radio" name="options" value="1" autocomplete="off" checked> Front Camera
