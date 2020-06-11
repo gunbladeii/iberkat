e2a80865fcce2012c807573224217a6f0c95d334
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-06-11 20:38:50 +0800

    Update test2.php

--- test2.php
+++ test2.php
@@ -34,15 +34,19 @@
 
       Instascan.Camera.getCameras().then(function (cameras) {
 
-        if (cameras.length > 0) {
-
-          scanner.start(cameras[1]);
-
-        } else {
-
-          console.error('No cameras found.');
-
+       if (cameras.length > 0) {
+    var selectedCam = cameras[0];
+    $.each(cameras, (i, c) => {
+        if (c.name.indexOf('back') != -1) {
+            selectedCam = c;
+            return false;
         }
+    });
+
+    scanner.start(selectedCam);
+} else {
+    console.error('No cameras found.');
+}
 
       }).catch(function (e) {
 
