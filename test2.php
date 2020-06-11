60552c75d6f7fde64b177067232275a20ad956f2
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-06-11 21:41:11 +0800

    Update test2.php

--- test2.php
+++ test2.php
@@ -34,7 +34,7 @@
       <div class="col">
         <script src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js"></script>
        <div style="col-sm-12;">
-          <video id="preview" class="mw-100"></video>
+          <video id="preview" class="mw-100 hw-100"></video>
         </div>
 
          <script type="text/javascript">
