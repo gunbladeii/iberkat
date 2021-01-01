d610d5cf188db6f423236d596fec8814ca2d2800
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2021-01-01 23:14:44 +0800

    Update liveViewPayroll.php

--- adminAFM/liveViewPayroll.php
+++ adminAFM/liveViewPayroll.php
@@ -10,7 +10,7 @@ if (isset($_SESSION['user'])) {
   $colname_Recordset2 = $_SESSION['user'];
 }
 
-$Recordset2 = $mysqli->query("SELECT attendance.nama, attendance.noIC, attendance.date AS date, attendance.stationCode, stationName.name AS stationName, COUNT(attendance.date) AS totalDay , attendance.month FROM attendance INNER JOIN stationName ON attendance.stationCode = stationName.stationCode WHERE attendance.timeOut IS NOT NULL GROUP BY attendance.noIC, attendance.month ORDER BY attendance.month ASC");
+$Recordset2 = $mysqli->query("SELECT attendance.nama, attendance.year, attendance.noIC, attendance.date AS date, attendance.stationCode, stationName.name AS stationName, COUNT(attendance.date) AS totalDay , attendance.month FROM attendance INNER JOIN stationName ON attendance.stationCode = stationName.stationCode WHERE attendance.timeOut IS NOT NULL GROUP BY attendance.noIC, attendance.month, attendance.year ORDER BY attendance.year,attendance.month ASC");
 $row_Recordset2 = mysqli_fetch_assoc($Recordset2);
 $totalRows_Recordset2 = mysqli_num_rows($Recordset2);
 
@@ -28,6 +28,7 @@ $a=1;
                   <th>Full Name</th>
                   <th>Station</th>
                   <th>Month</th>
+                  <th>Year</th>
                   <th>Total working days</th>
                 </tr>
                 </thead>
@@ -38,6 +39,7 @@ $a=1;
 	            <td> <span data-toggle="modal" data-target="#viewRiderModal" data-whatever="<?php echo $row_Recordset2['noIC'];?>" data-whatever2="<?php echo $row_Recordset2['month'];?>" class="badge badge-primary" role="button" aria-pressed="true"><?php echo ucwords($row_Recordset2['nama']);?></span></td>	
 	            <td><?php echo $row_Recordset2['stationName'];?></td>
 	            <td><span class="badge badge-info"><?php $date=date_create($row_Recordset2['date']);echo date_format($date,"F");?></span></td>
+               <td><?php echo $row_Recordset2['year'];?></td>
                 <td class="d-sm-inline-flex"><span class="badge badge-warning"><?php echo $row_Recordset2['totalDay'];?></span></td>	
 	            </tr>
                 <?php } while ($row_Recordset2 = mysqli_fetch_assoc($Recordset2));?>
@@ -48,6 +50,7 @@ $a=1;
                   <th>Full Name</th>
                   <th>Station</th>
                   <th>Month</th>
+                  <th>Year</th>
                   <th>Total working days</th>
                 </tr>
                 </tfoot>
